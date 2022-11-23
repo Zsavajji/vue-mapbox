0b3acf5c8b2d65d2f2a2e7da9225cbe5667a5e15
Andrea Faggin <andrea.faggin@we-go.it>
2022-11-23 15:50:22 +0100

    Fixed geojson methods

--- src/components/layer/GeojsonLayer.js
+++ src/components/layer/GeojsonLayer.js
@@ -90,7 +90,7 @@ export default {
     if (this.source) {
       this.$watch(
         "source.data",
-        function(next) {
+        function (next) {
           if (this.initial) return;
           this.mapSource.setData(next);
         },
@@ -145,14 +145,14 @@ export default {
 
     setFeatureState(featureId, state) {
       if (this.map) {
-        const params = { id: featureId, source: this.source };
+        const params = { id: featureId, source: this.sourceId };
         return this.map.setFeatureState(params, state);
       }
     },
 
     getFeatureState(featureId) {
       if (this.map) {
-        const params = { id: featureId, source: this.source };
+        const params = { id: featureId, source: this.sourceId };
         return this.map.getFeatureState(params);
       }
     },
@@ -161,7 +161,7 @@ export default {
       if (this.map) {
         const params = {
           id: featureId,
-          source: this.source,
+          source: this.sourceId,
           sourceLayer
         };
         return this.map.removeFeatureState(params, key);
